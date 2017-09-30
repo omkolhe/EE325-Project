c0993d51300d2895040d3c0ddf90b04dfdb955a2
omkolhe <omkolhe026@gmail.com>
2017-09-30 18:50:30 +0530

    Riffle shuffle -selection of k done - plot remaining

--- Riffle/riffle.cpp
+++ Riffle/riffle.cpp
@@ -4,36 +4,54 @@
 #include <math.h>
 using namespace std;
 
+double fact_n;
+
 double factorial(int n); // To calculate the factorial
 double nCk(int n,int k);
 int rand();
 
 int main()
 {
-  int n = 6;
-  cout << nCk(4,2) << endl;
-  srand((unsigned)time(0));
-  double k_rand = rand();
-  cout << k_rand << endl;
-  k_rand /=32767.0;
-//  k_rand = 0.1;
-  cout << k_rand << endl;
-  int k = 0;
-  int i = 0;
-  double nck = 0;
-  for(i=0;i<n;i++)
+  int n = 18;
+  fact_n=factorial(n);
+
+  int per[n+1];
+  for(int j=0;j<n+1;j++)
   {
-    cout << nck << " ";
-    double nck_1 = nCk(n,i+1)/(pow(2.0,n));
-    cout << nck_1 << endl;
-    if(k_rand>=nck && k_rand<(nck_1+nck))
-    {
-      break;
-    }
-    nck += nck_1;
+    per[j]=0;
   }
-  k = i+1;
-  cout << k << endl;
+
+  srand((unsigned)time(0));
+
+
+  for(int xx=0;xx<999999;xx++)
+  {
+    double k_rand = rand();
+    //cout << k_rand << endl;
+    k_rand /=32767.0;
+    int k = 0;
+    int i = 0;
+    double nck = 0;
+    for(i=0;i<=n;i++)
+    {
+      double nck_1 = nCk(n,i)/(pow(2.0,n));
+      if(k_rand>=nck && k_rand<(nck_1+nck))
+      {
+        break;
+      }
+      nck += nck_1;
+    }
+    k = i;
+    per[k]++;
+  }
+
+
+  for(int j=0;j<n+1;j++)
+  {
+    cout << j << " : " << per[j] << endl;
+  }
+
+
   return 0;
 }
 
@@ -51,21 +69,6 @@ double factorial(int n)
 double nCk(int n,int k)
 {
   double ans;
-  ans = factorial(n)/(factorial(k)*factorial(n-k));
+  ans = fact_n/(factorial(k)*factorial(n-k));
   return ans;
 }
-
-/**  if(i==0)
-  {
-    if(k_rand>=0 && k_rand<nck_1)
-    {
-      break;
-    }
-  }
-  else
-  {
-    if(k_rand>=nck && k_rand<(nck_1+nck))
-    {
-      break;
-    }
-  } */
